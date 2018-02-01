be82068be99c69e3c3055f3064b25ba63e8ed646
Grzegorz Gajos <gagreg@gmail.com>
2018-02-01 14:48:47 +0100

    Closing old version of the page

--- assets/images/generator/generate.bat
+++
@@ -1,7 +0,0 @@
-call m
-set filename=%2.png
-rem if exist %filename% goto end
-convert -background gray90 -fill black -font Hack-Regular -size 220x220 -gravity center label:%1 image.png
-convert -caption "ggajos.com" -font Hack-Regular image.png -thumbnail 220x220 -bordercolor #fafafa -background gray40 +polaroid %filename%
-del image.png
-rem :end
--- assets/images/generator/rebuild.bat
+++
@@ -1,1 +0,0 @@
-call generate "Laptop vs desktop\nintel i7 4 vs 6 cores" "2015-11-01"
